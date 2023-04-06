48cfb624211f03b619cad344466f8c22a9df921f
Davide <95879199+davidevigolo@users.noreply.github.com>
2023-04-06 10:34:39 +0200

    Add files via upload

---
+++ 404.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../style/dashboard.css">
+    <title>Errore 404!</title>
+    <style>
+    h1{
+        animation-name:testo;
+        animation-duration:5s;
+        animation-delay:-.2s;
+        font-size: 100px;
+
+    }
+    h2{
+        animation-name:testo;
+        animation-duration:5s;
+        animation-delay:-.2s;
+        font-size: 50px;
+    }
+    a{
+        text-decoration: none;
+    }
+    @keyframes testo{
+        from {color:#242424;}
+        to  {color:white;}
+    }
+    </style>
+</head>
+<body>
+<h1>Errore 404!</h1><br><br>
+<a href="https://bicicletta22235id.altervista.org"><h2>Torna al Login</h2></a>
+    <?php 
+        
+    ?>
+</body>
+</html>
---
+++ fiveserver.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    highlight: true, // enable highlight feature
+    injectBody: true, // enable instant update
+    remoteLogs: true, // enable remoteLogs
+    remoteLogs: "yellow", // enable remoteLogs and use the color yellow
+    injectCss: false, // disable injecting css
+    navigate: true, // enable auto-navigation
+    php: "D:\\xampp\\xampp\\php\\php.exe"   // Windows
+}
